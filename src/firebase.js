211362c3a52693f39dbbd69a0d630cf31a1483c3
reubenwedson <reubenwedson@gmail.com>
2020-09-09 17:20:55 +0300

    adding firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBnKT3JhhDpFwEh_Rl6hw1c1PKljOT3c4g",
+    authDomain: "clone-8b8ac.firebaseapp.com",
+    databaseURL: "https://clone-8b8ac.firebaseio.com",
+    projectId: "clone-8b8ac",
+    storageBucket: "clone-8b8ac.appspot.com",
+    messagingSenderId: "885965014048",
+    appId: "1:885965014048:web:8e53e1040b6c949b166845",
+    measurementId: "G-12TGCGTDX1"
+  };
