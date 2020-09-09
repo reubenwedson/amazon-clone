1c85543f49f1d9a4df813d3d93ded95c104dd6db
reubenwedson <reubenwedson@gmail.com>
2020-09-09 19:02:11 +0300

    adding header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Amazon-Clone</h1>
+      <Header/>
     </div>
   );
 }
