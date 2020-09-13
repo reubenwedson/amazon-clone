82af5eb58b2a875019bc812791722000ad0a5ddf
reubenwedson <reubenwedson@gmail.com>
2020-09-13 10:52:27 +0300

    Adding state to product component

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider'
 
-function Product({ title, image, price, rating}) {
+function Product({ id,title, image, price, rating}) {
+
+    const [{basket}, dispatch] = useStateValue;
+
+    const addToBasket = () => {
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+              id: id,
+              title: title,
+              image: image,
+              price: price,
+              rating: rating,
+            },
+          });
+    }
+
+
     return (
         <div className="product">
             <div className="product__info">
@@ -24,7 +42,7 @@ function Product({ title, image, price, rating}) {
                 src={image}
                 alt="product__image"/>
 
-                <button>Add to Basket</button>
+                <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
