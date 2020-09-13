e92667606c0dc775099691ba6a262a5eb5c6613a
reubenwedson <reubenwedson@gmail.com>
2020-09-13 10:27:01 +0300

    Adding checkout , subtotal and state provider

--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,18 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
+      <Link to="/">
       <img
         className="header__logo"
         src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         alt="header__logo"
       />
+      </Link>
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon"/>
@@ -28,11 +31,12 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-
+        <Link to="/checkout">
         <div className="header__optionBacket">
             <ShoppingBasketIcon />
           <span className="header__optionLineTwo header__backetCount">0</span>
         </div>
+        </Link>
       </div>
     </div>
   );
