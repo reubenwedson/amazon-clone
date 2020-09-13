bc4751dc17324cf07024de436e7575b10e1c66e4
reubenwedson <reubenwedson@gmail.com>
2020-09-13 15:59:58 +0300

    Handling registering and login user

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase/app'
+import  'firebase/auth'
+import  'firebase/firestore'
+
 const firebaseConfig = {
     apiKey: "AIzaSyBnKT3JhhDpFwEh_Rl6hw1c1PKljOT3c4g",
     authDomain: "clone-8b8ac.firebaseapp.com",
@@ -8,3 +12,11 @@ const firebaseConfig = {
     appId: "1:885965014048:web:8e53e1040b6c949b166845",
     measurementId: "G-12TGCGTDX1"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export {db, auth};
